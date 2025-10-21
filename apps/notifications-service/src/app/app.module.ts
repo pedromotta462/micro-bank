882a5b710da39342c828abb2e1582687c661b0fd
pedromotta462 <pedromotta1d2015@gmail.com>
2025-10-21 19:32:31 -0300

    Fix: update schema.prisma to include binaryTargets for Prisma Client and enhance docker-compose.yml with additional environment variables for services

--- apps/notifications-service/src/app/app.module.ts
+++ apps/notifications-service/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotificationsModule } from './notifications/modules/notifications.module';
+import { LoggerModule } from './common/logger/logger.module';
 
 @Module({
   imports: [
+    LoggerModule,
     NotificationsModule,
   ],
   controllers: [AppController],
