927592d4eb0ae1685c05fc3ff905e0ea961faf09
Pedro Motta <pedromotta1d2015@gmail.com>
2025-10-21 20:54:42 -0300

    Release fix update webpack configuration to include generated assets for both transactions and users services (#28)
    * Fix: update webpack configuration to include generated assets for both transactions and users services

--- apps/users-service/webpack.config.js
+++ apps/users-service/webpack.config.js
@@ -14,7 +14,14 @@ module.exports = {
       compiler: 'tsc',
       main: './src/main.ts',
       tsConfig: './tsconfig.app.json',
-      assets: ['./src/assets'],
+      assets: [
+        './src/assets',
+        {
+          glob: '**/*',
+          input: './generated',
+          output: './generated',
+        },
+      ],
       optimization: false,
       outputHashing: 'none',
       generatePackageJson: true,
